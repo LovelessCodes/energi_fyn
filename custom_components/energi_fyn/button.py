534e84d4ea49cf4391a935776de28a134ad64160
Mike Kold Hermann <mkh@cleanmanager.dk>
2026-04-01 08:24:27 +0200

    feat: add refresh button for Energi Fyn

---
+++ custom_components/energi_fyn/button.py
@@ -0,0 +1,22 @@
+"""Buttons for Energi Fyn."""
+
+from homeassistant.components.button import ButtonEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .const import DOMAIN
+
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    coordinator = hass.data[DOMAIN][config_entry.entry_id]["coordinator"]
+    async_add_entities([EnergiFynRefreshButton(coordinator)])
+
+
+class EnergiFynRefreshButton(CoordinatorEntity, ButtonEntity):
+    def __init__(self, coordinator):
+        super().__init__(coordinator)
+        self._attr_name = "Energi Fyn Refresh"
+        self._attr_unique_id = "energi_fyn_refresh"
+        self._attr_icon = "mdi:refresh"
+
+    async def async_press(self) -> None:
+        await self.coordinator.async_request_refresh()
